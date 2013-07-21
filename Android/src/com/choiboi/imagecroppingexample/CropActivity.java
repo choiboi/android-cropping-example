89264248067474d4a3231f6c00c9df880c67a5d1
choiboi <james.choiboi@gmail.com>
2013-07-21 01:15:04 -0400

    Most part of layout done.

---
+++ Android/src/com/choiboi/imagecroppingexample/CropActivity.java
@@ -0,0 +1,22 @@
+package com.choiboi.imagecroppingexample;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CropActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crop);
+    }
+    
+    public void onCropImageButton(View v) {
+        
+    }
+    
+    public void onChangeTemplateButton(View v) {
+        
+    }
+}
--- Android/src/com/choiboi/imagecroppingexample/MainActivity.java
+++ Android/src/com/choiboi/imagecroppingexample/MainActivity.java
@@ -1,7 +1,9 @@
 package com.choiboi.imagecroppingexample;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends Activity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    public void onStartCropButton(View v) {
+        Intent intent = new Intent(this, CropActivity.class);
+        startActivity(intent);
     }
 }
