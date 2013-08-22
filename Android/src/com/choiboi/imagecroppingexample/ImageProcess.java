43a43b6660f4a92e8adaf26da1b67bff70ad9703
choiboi <james.choiboi@gmail.com>
2013-08-22 18:09:18 -0400

    Created newer more efficient crop algorithm.

--- Android/src/com/choiboi/imagecroppingexample/ImageProcess.java
+++ Android/src/com/choiboi/imagecroppingexample/ImageProcess.java
@@ -156,4 +156,115 @@ public class ImageProcess {
         bm.recycle();
         return finalBm;
     }
+    
+    public static Bitmap cropImageVer2(Bitmap img, Bitmap templateImage, int width, int height) {
+        // Merge two images together.
+        Bitmap bm = Bitmap.createBitmap(img.getWidth(), img.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas combineImg = new Canvas(bm);
+        combineImg.drawBitmap(img, 0f, 0f, null);
+        combineImg.drawBitmap(templateImage, 0f, 0f, null);
+        
+        // Create new blank ARGB bitmap.
+        Bitmap finalBm = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+        Canvas finalBmCanvas = new Canvas(finalBm);
+        finalBmCanvas.drawColor(Color.TRANSPARENT);
+        
+        // Get the coordinates for the middle of bm.
+        int hMid = bm.getHeight() / 2;
+        int wMid = bm.getWidth() / 2;
+        int hfMid = finalBm.getHeight() / 2;
+        int wfMid = finalBm.getWidth() / 2;
+
+        int y2 = hfMid;
+        int x2 = wfMid;
+
+        for (int y = hMid; y >= 0; y--) {
+            // Check Upper-left section of combineImg.
+            for (int x = wMid; x >= 0; x--) {
+                if (x2 < 0) {
+                    break;
+                }
+
+                int px = bm.getPixel(x, y);
+                if (Color.red(px) == 234 && Color.green(px) == 157 && Color.blue(px) == 33) {
+                    break;
+                } else {
+                    finalBm.setPixel(x2, y2, px);
+                }
+                x2--;
+            }
+            
+            // Check upper-right section of combineImage.
+            x2 = wfMid;
+            for (int x = wMid; x < bm.getWidth(); x++) {
+                if (x2 >= finalBm.getWidth()) {
+                    break;
+                }
+
+                int px = bm.getPixel(x, y);
+                if (Color.red(px) == 234 && Color.green(px) == 157 && Color.blue(px) == 33) {
+                    break;
+                } else {
+                    finalBm.setPixel(x2, y2, px);
+                }
+                x2++;
+            }
+            
+            int px = bm.getPixel(wMid, y);
+            if (Color.red(px) == 234 && Color.green(px) == 157 && Color.blue(px) == 33) {
+                break;
+            } 
+            x2 = wfMid;
+            y2--;
+        }
+
+        x2 = wfMid;
+        y2 = hfMid;
+        for (int y = hMid; y <= bm.getHeight(); y++) {
+            // Check bottom-left section of combineImage.
+            for (int x = wMid; x >= 0; x--) {
+                if (x2 < 0) {
+                    break;
+                }
+
+                int px = bm.getPixel(x, y);
+                if (Color.red(px) == 234 && Color.green(px) == 157 && Color.blue(px) == 33) {
+                    break;
+                } else {
+                    finalBm.setPixel(x2, y2, px);
+                }
+                x2--;
+            }
+
+            // Check bottom-right section of combineImage.
+            x2 = wfMid;
+            for (int x = wMid; x < bm.getWidth(); x++) {
+                if (x2 >= finalBm.getWidth()) {
+                    break;
+                }
+
+                int px = bm.getPixel(x, y);
+                if (Color.red(px) == 234 && Color.green(px) == 157 && Color.blue(px) == 33) {
+                    break;
+                } else {
+                    finalBm.setPixel(x2, y2, px);
+                }
+                x2++;
+            }
+            
+            int px = bm.getPixel(wMid, y);
+            if (Color.red(px) == 234 && Color.green(px) == 157 && Color.blue(px) == 33) {
+                break;
+            } 
+            
+            x2 = wfMid;
+            y2++;
+        }
+        
+        // Get rid of images that we finished with to save memory.
+        img.recycle();
+        templateImage.recycle();
+        bm.recycle();
+        return finalBm;
+    }
 }
